0a590ce23db42d88d6a9cbe22059dd997ee90cfc
GRLuis1565 <luisggr2004@gmail.com>
2025-11-02 18:27:00 -0600

    Commit inicial: Archivos completos de la aplicación Crud Razor Page

---
+++ CrudRazorApp/Pages/Tareas/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrudRazorApp.Pages.Tareas
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
